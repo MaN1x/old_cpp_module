123e94b8bc98f9887af017c571257163c6c07901
Maxim <mister.kozlov.maxim@yandex.ru>
2022-04-09 20:48:08 +0300

    add ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+//
+// Created by maxim on 09.04.2022.
+//
+
+#include <iostream>
+#include <cstring>
+
+void str_up(char **str)
+{
+	int i;
+
+	i = strlen(*str);
+	while (i-- > 0)
+	{
+		if ((*str)[i] >= 'a' && (*str)[i] <= 'z')
+			(*str)[i] = (*str)[i] - ' ';
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while(argc-- > 1)
+		{
+			str_up(&argv[1]);
+			std::cout << argv[1];
+			argv++;
+		}
+	}
+	std::cout << std::endl;
+	return(0);
+}
+
